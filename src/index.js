abc624eb5f554709f0e5b27625a24775b66da083
JerryG09 <phemmy09@gmail.com>
2020-01-10 16:27:06 +0100

    initial commit

---
+++ src/index.js
@@ -0,0 +1,39 @@
+import { GraphQLServer } from 'graphql-yoga'
+
+// Scalar types - String, Boolean, Int, Float, ID
+
+// Type definitions (schema)
+const typeDefs = `
+    type Query {
+        me: User!
+    }
+
+    type User {
+        id: ID!
+        name: String!
+        email: String!
+        age: Int
+    }
+`
+
+// Resolvers
+const resolvers = {
+    Query: {
+        me() {
+            return {
+                id: '123456',
+                name: 'Mike',
+                email: 'mike@gmail.com'
+            }
+        }
+    }
+}
+
+const server = new GraphQLServer({
+    typeDefs,
+    resolvers
+})
+
+server.start(() => {
+    console.log('The server is up!')
+})
