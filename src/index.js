2e68e5dcca5675f77067f3dd4f347a2f10f4e371
JerryG09 <phemmy09@gmail.com>
2020-01-13 07:45:48 +0100

    added deletePost and deleteComment mutation

--- src/index.js
+++ src/index.js
@@ -74,7 +74,9 @@ const typeDefs = `
         createUser(data: CreateUserInput!): User!
         deleteUser(id: ID!): User!
         createPost(data: CreatePostInput!): Post!
+        delelePost(id: ID!): Post!
         createComment(data: CreateCommentInput!): Comment!
+        deleteComment(id: ID!): Comment!
     }
 
     input CreateUserInput {
@@ -216,6 +218,19 @@ const resolvers = {
 
             return post
         },
+        delelePost(parent, args, ctx, info) {
+            const  postIndex = posts.findIndex(post => post.id === args.id)
+
+            if (postIndex === -1) {
+                throw new Error('Post not found')
+            } 
+            
+            const deletedPosts = posts.splice(postIndex, 1)
+
+            comments = comments.filter(comment => comment.post !== args.id)
+
+            return deletedPosts[0]
+        },
         createComment(parent, args, ctx, info) {
             const userExits = users.some(user => user.id === args.data.author)
             const postExists = posts.some(post => post.id === args.data.post && post.data.published)
@@ -236,6 +251,17 @@ const resolvers = {
             comments.push(comment);
 
             return comment;
+        },
+        deleteComment(parent, args, ctx, info) {
+            const commentIndex = comments.findIndex(comment => comment.id=== args.id)
+
+            if (commentIndex === -1) {
+                throw new Error('Comment not found')
+            }
+
+            const deletedComments = comments.splice(commentIndex, 1);
+
+            return deletedComments[0];
         }
     },
     Post: {
